d3c43316579b715e2b89272c2bab404a6c1b31b7
Your Name <pentius00@gmail.com>
2025-08-24 23:41:14 -0400

    🔧 Fix PyPI upload: Update package name to avoid TestPyPI conflicts

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def get_requirements():
 
 setup(
     # Package Identity
-    name="nis-protocol-v321-organica",
+    name="nis-protocol-v321-organica-fixed",
     version=get_version(),
     author="Organica AI Solutions",
     author_email="developers@organicaai.com",
